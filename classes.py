34bfd85775afb79f4b4e666837c745faad178f7c
samuel jones <sojonesey@gmail.com>
2021-03-02 11:14:40 +0000

    first commit

---
+++ classes.py
@@ -0,0 +1,178 @@
+import pygame
+from config import *  # pylint: disable=W0614
+import pygame.freetype
+from math import floor
+
+class Tile:
+
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+        self.txt = None
+        self.gridX = int(x/GRIDTILESIZE)
+        self.gridY = int(y/GRIDTILESIZE)
+        self.boundsRect = pygame.Rect((self.x, self.y, GRIDTILESIZE, GRIDTILESIZE))
+        self.selected = False
+        self.hasLine = False
+
+    def __repr__(self):
+        return(f"Tile coords({self.gridX},{self.gridY})")
+
+    def isCollide(self, screen, pos):
+        if self.boundsRect.collidepoint(pos):
+            self.selected = True            
+            return True
+        else:
+            self.selected = False
+            return False
+
+    def draw(self, screen):
+        if self.selected:
+            pygame.draw.rect(screen, HIGHLIGHT_BLUE, self.boundsRect)
+
+class Atom:
+
+    def __init__(self, x, y, txt):
+        self.x = x
+        self.y = y
+        self.txt = txt
+        self.font = pygame.freetype.Font('MonospaceTypewriter.ttf', size=int(FONT_SIZE*GRIDTILESIZE/100))
+    
+    def __repr__(self):
+        return(f"Free Atom of text {self.txt}")
+
+    def update(self, pos):
+        self.x, self.y = pos
+
+    def draw(self, screen):
+        self.font.render_to(screen, (self.x, self.y), self.txt, BLACK)
+
+
+class TiledAtom(Tile, Atom):
+    
+    def __init__(self, x, y, txt):
+        Tile.__init__(self, x, y)
+        Atom.__init__(self, x, y, txt)
+        fx = self.font.get_rect(txt).width 
+        fy = self.font.get_rect(txt).height
+        tx, ty = self.boundsRect.center
+        self.fontOffsetX = int((tx-x) - (fx/2))
+        self.fontOffsetY = int((ty-y) - (fy/2))
+        self.txtRect = pygame.Rect(0,0,0,0)
+
+
+    def __repr__(self):
+        return(f"Tiled Atom grid {self.gridX}:{self.gridY}, txt {self.txt}")
+
+    def reverseTxt(self):
+        nS = self.txt[::-1]
+        self.txt = nS
+
+    def draw(self, screen):
+        if self.selected:
+            self.colour = HIGHLIGHT_BLUE
+        else:
+            self.colour = BLACK
+        pygame.draw.rect(screen, WHITE, self.txtRect)
+        self.txtRect = self.font.render_to(screen, (self.x + self.fontOffsetX, self.y + self.fontOffsetY), self.txt, self.colour)
+        self.txtRect.left = self.x + self.fontOffsetX
+        self.txtRect.top = self.y + self.fontOffsetY       
+
+class OHGroup(TiledAtom):
+
+    def __init__(self, x, y, txt = "OH"):
+        TiledAtom.__init__(self, x, y, txt)
+        self.txtRect = None
+    
+    def __repr__(self):
+        return(f"OH group at {self.gridX}, {self.gridY}")
+
+class Dot:
+
+    def __init__(self, x = 0, y = 0):
+        self.x = x
+        self.y = y
+        
+    def __repr__(self):
+        return("This isn't nessacry - what is spell?")
+
+    def update(self, pos):
+        self.x, self.y = pos
+
+    def draw(self, screen):
+        pygame.draw.circle(screen, BLACK, (self.x, self.y), DOT_RADIUS)
+
+
+class Line:
+
+    def __init__(self, start, end = (0,0), numOfBonds = 1):
+        self.startPos = start
+        self.endPos = end
+        self.selected = False
+        self.startGridX = floor(start[0]/GRIDTILESIZE)
+        self.startGridY = floor(start[1]/GRIDTILESIZE)
+        self.endGridX = floor(end[0]/GRIDTILESIZE)
+        self.endGridY = floor(end[1]/GRIDTILESIZE)
+        self.numOfBonds = 1    #As in 1 is a single bond, 2 double, 3 triple, AND THE FORBIDDEN 4 QUADRUPLE BOND
+        self.lineRect = None
+        self.colour = BLACK
+        self.orentation = None
+        self.startCollide = False
+        self.endCollide = False
+        x1, y1 = start
+        x2, y2 = end
+        if x1 - x2 == 0:
+            self.orentation = "Vertical"
+        elif y1 - y2 == 0:
+            self.orentation = "Horizontal"
+        else:
+            self.orentation = "Diagonal" 
+
+    def __repr__(self):
+        return(f"Line from {self.startPos} to {self.endPos}")
+
+    def update(self, pos):
+        self.endPos = pos
+    
+    def isCollide(self, pos):
+        if self.lineRect.collidepoint(pos):
+            self.colour = HIGHLIGHT_BLUE
+            return True
+        else:
+            self.colour = BLACK
+            return False
+        
+    def endsCollide(self, grid, screen):
+        if grid[self.startGridY][self.startGridX].txt != None:
+            grid[self.startGridY][self.startGridX].draw(screen)
+        if grid[self.endGridY][self.endGridX].txt != None:
+            grid[self.endGridY][self.endGridX].draw(screen)
+        
+    def update_Bond_Number(self):
+        self.numOfBonds += 1
+        if self.numOfBonds > BOND_LIMIT:
+            self.numOfBonds = 1
+
+    def draw(self, screen):
+        if self.numOfBonds == 1 or self.numOfBonds == 3:
+            self.lineRect = pygame.draw.line(screen, self.colour, self.startPos, self.endPos, int(LINEWIDTH*GRIDTILESIZE/100))
+        if self.numOfBonds != 1:
+            x1, y1 = self.startPos
+            x2, y2 = self.endPos 
+            if self.numOfBonds == 2 or self.numOfBonds == 4:            
+                offset = int(BASE_BOND_OFFSET_DOUBLE*GRIDTILESIZE/100)
+            if self.numOfBonds == 3:
+                offset = int(BASE_BOND_OFFSET_TRIPLE*GRIDTILESIZE/100)
+            if self.orentation == "Vertical":
+                pygame.draw.line(screen, self.colour, (x1-offset, y1), (x2-offset,y2), int(LINEWIDTH*GRIDTILESIZE/100))
+                pygame.draw.line(screen, self.colour, (x1+offset, y1), (x2+offset, y2), int(LINEWIDTH*GRIDTILESIZE/100))
+            else:
+                pygame.draw.line(screen, self.colour, (x1, y1-offset), (x2, y2-offset), int(LINEWIDTH*GRIDTILESIZE/100))
+                pygame.draw.line(screen, self.colour, (x1, y1+offset), (x2, y2+offset), int(LINEWIDTH*GRIDTILESIZE/100))
+            if FORBIDDEN and self.numOfBonds == 4:
+                if self.orentation == "Vertical":
+                    pygame.draw.line(screen, self.colour, (x1-int(0.5*offset), y1), (x2-int(0.5*offset),y2), int(LINEWIDTH*GRIDTILESIZE/100))
+                    pygame.draw.line(screen, self.colour, (x1+int(0.5*offset), y1), (x2+int(0.5*offset), y2), int(LINEWIDTH*GRIDTILESIZE/100))
+                    pygame.draw.line(screen, self.colour, (x1-(2*offset), y1), (x2-(2*offset),y2), int(LINEWIDTH*GRIDTILESIZE/100))
+                    pygame.draw.line(screen, self.colour, (x1+(2*offset), y1), (x2+(2*offset), y2), int(LINEWIDTH*GRIDTILESIZE/100))
+        
---
+++ main.py
@@ -0,0 +1,165 @@
+import pygame
+from classes import *  # pylint: disable=W0614
+from config import *  # pylint: disable=W0614
+import time
+from math import floor
+
+SCREENX = 1000
+SCREENY = 600
+
+pygame.init()  # pylint: disable=no-member
+
+if not FORBIDDEN and BOND_LIMIT > 3:
+    raise Exception("Can't have more than a triple bond...........")
+
+def makeGrid():
+    if SCREENX % GRIDTILESIZE != 0 or SCREENY % GRIDTILESIZE != 0:
+        raise Exception("Screen not divisble by TileSize")
+    masterGrid = []
+    for y in range(0, SCREENY, GRIDTILESIZE):
+        row = []
+        for x in range(0, SCREENX, GRIDTILESIZE):
+            row.append(Tile(x, y))  
+        masterGrid.append(row)
+        del row
+
+    return masterGrid
+
+def main():
+
+    screen = pygame.display.set_mode((SCREENX,SCREENY))
+    masterGrid = makeGrid()
+    entities = []
+    free_Entity = None
+    newAtom = False
+    newLine = False
+    drawLine = False
+    bondEdit = False
+    reverseTxt = False
+    erase = False
+
+    running = True
+
+    while running:
+
+        start = time.time()
+        screen.fill(WHITE)
+
+        for row in masterGrid:
+            for obj in row:
+                if not bondEdit:
+                    if not erase:
+                        obj.isCollide(screen, pygame.mouse.get_pos()) 
+                    elif erase and obj.txt != None:
+                        obj.isCollide(screen, pygame.mouse.get_pos())
+                obj.draw(screen)    
+
+        for obj in entities:
+            obj.draw(screen)
+            obj.endsCollide(masterGrid, screen)
+            if bondEdit or erase:
+                obj.isCollide(pygame.mouse.get_pos())
+
+
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:   # pylint: disable=no-member
+                running = False
+            if event.type == pygame.MOUSEBUTTONDOWN:        # pylint: disable=no-member   
+                left, middle, right = pygame.mouse.get_pressed()
+                #if (left and newAtom) or (left and newLine) or (left and drawLine) or (left and reverseTxt):
+                if left:
+                    if bondEdit or erase:
+                        for obj in entities:
+                            if obj.isCollide(pygame.mouse.get_pos()):
+                                if bondEdit:
+                                    obj.update_Bond_Number()
+                                elif erase:
+                                    entities.remove(obj)
+                    if not bondEdit:
+                        x, y = pygame.mouse.get_pos()                        
+                        for ly, row in enumerate(masterGrid):
+                            for lx, obj in enumerate(row):
+                                if obj.isCollide(screen, (x,y)):
+                                    if newAtom:
+                                        masterGrid[ly][lx] = TiledAtom(obj.x, obj.y, free_Entity.txt)
+                                        free_Entity = None            
+                                        newAtom = False
+                                    elif newLine and not drawLine:
+                                        free_Entity = Line(obj.boundsRect.center)
+                                        obj.hasLine = True
+                                        drawLine = True
+                                    elif newLine and drawLine:
+                                        entities.append(Line(free_Entity.startPos, obj.boundsRect.center))
+                                        obj.hasLine = True
+                                        free_Entity = Line(obj.boundsRect.center)
+                                    elif reverseTxt:
+                                        obj.reverseTxt()
+                                    elif erase:
+                                        masterGrid[obj.gridY][obj.gridX] = Tile(floor(x/GRIDTILESIZE)*GRIDTILESIZE, floor(y/GRIDTILESIZE)*GRIDTILESIZE)                    
+                if right:
+                    newAtom = False
+                    newLine = False
+                    drawLine = False
+                    reverseTxt = False
+                    bondEdit = False
+                    erase = False
+                    free_Entity = None
+
+            if event.type == pygame.KEYDOWN:        # pylint: disable=no-member
+                free_Entity = None
+                newLine = False
+                drawLine = False
+                newAtom = False
+                bondEdit = False
+                reverseTxt = False
+                erase = False
+                if event.key == pygame.K_l:     # pylint: disable=no-member
+                    if newLine:
+                        newLine = False
+                        free_Entity = None
+                    else:
+                        newLine = True
+                        free_Entity = Dot()              
+                elif event.key == pygame.K_c:       # pylint: disable=no-member
+                    free_Entity = Atom(0,0, "C")
+                    newAtom = True
+                elif event.key == pygame.K_o and event.mod & pygame.KMOD_SHIFT:     # pylint: disable=no-member
+                    free_Entity = Atom(0,0, "OH")
+                    newAtom = True
+                elif event.key == pygame.K_o:       # pylint: disable=no-member 
+                    free_Entity = Atom(0,0, "O")
+                    newAtom = True
+                elif event.key == pygame.K_n:       # pylint: disable=no-member
+                    free_Entity = Atom(0,0, "N")
+                    newAtom = True
+                elif event.key == pygame.K_b:       # pylint: disable=no-member
+                    if bondEdit:
+                        bondEdit = False
+                        for obj in entities:
+                            obj.colour = BLACK
+                    else:
+                        bondEdit = True
+                        for row in masterGrid:
+                            for obj in row: obj.selected = False
+                elif event.key == pygame.K_r:       # pylint: disable=no-member
+                    reverseTxt = True
+                elif event.key == pygame.K_e:       # pylint: disable=no-member
+                    for row in masterGrid:
+                        for obj in row: obj.selected = False
+                    erase = True
+
+
+
+        if free_Entity != None:
+            free_Entity.update(pygame.mouse.get_pos())
+            free_Entity.draw(screen)
+
+        pygame.display.flip()
+        try:
+            pygame.display.set_caption(str(int(1/(time.time()-start))))
+        except:
+            pass
+
+if __name__ == "__main__":
+    main()
